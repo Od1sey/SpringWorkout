3d06e02271c1204236c81337fa3cad734f2bb798
Od1sey <doingmybestsometime@gmail.com>
2026-07-16 02:36:04 +0300

    added bookRepo and logic around it

--- src/main/java/com/contoller/BookController.java
+++ src/main/java/com/contoller/BookController.java
@@ -1,45 +1,114 @@
 package com.contoller;
 
+import com.entity.Book;
 import com.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.List;
 import java.util.Scanner;
 
 @Controller
 public class BookController {
 
-    @Autowired
-    BookService bookService;
+    private final BookService bookService;
+    private final Scanner scanner = new Scanner(System.in);
+    private boolean isRunning = false;
 
-    public void startMenu(){
-        Scanner scanner = new Scanner(System.in);
-        boolean isRunning = true;
+    @Autowired
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    public void startMainMenu(){
+        isRunning = true;
         while(isRunning){
             System.out.print("""
-                    === Menu ===:\s
+                    
+                    === Menu ===\s
                     1. Add record\s
                     2. List records\s
                     3. Find record\s
                     4. Exit\s
+                    ===========
+                    
                     """);
-            String input = scanner.nextLine();
-            switch (input.trim()){
-                case "1" -> System.out.println("One");
-                case "2" -> getAllRecords();
+            String input = scanner.nextLine().trim();
+            switch (input){
+                case "1" -> startNewRecordMenu();
+                case "2" -> printAllRecords();
+                case "3" -> startFindRecordMenu();
                 case "4" -> isRunning = false;
                 default -> System.out.println("Unknown command. Try again.");
             }
         }
     }
 
-    private void getAllRecords(){
-        List<String> records = bookService.getAllRecords();
-        if (records.isEmpty()) {
+
+    private void startNewRecordMenu(){
+        boolean isAddingNewRecord = true;
+        while(isAddingNewRecord){
+            System.out.print("""
+                    
+                    === Adding new record ===\s
+                    Please enter name of the book below
+                    or "return" to exit this menu
+                    
+                    =========================
+                    """);
+            String input = scanner.nextLine().trim();
+            if ("return".equals(input)){
+               isAddingNewRecord = false;
+            } else {
+                try {
+                    bookService.createNewRecord(input);
+                    System.out.printf("Book with name %s was added", input);
+                } catch (IllegalArgumentException ex) {
+                    System.out.println("\n" + ex.getMessage()+ "\n");
+                }
+            }
+        }
+    }
+
+    private void printAllRecords(){
+        displayList(bookService.getAllRecords());
+    }
+
+    private void startFindRecordMenu(){
+        boolean isFindingRecord = true;
+        while (isFindingRecord){
+            System.out.print("""
+                    
+                    === Lookup for a record ===\s
+                    Please enter name of the book below
+                    or "return" to exit this menu
+                    ===========================
+
+                    """);
+            String input = scanner.nextLine().trim();
+            if ("return".equals(input)){
+                isFindingRecord = false;
+            } else {
+                try{
+                    displayList(bookService.getRecordsByQuery(input));
+                } catch (IllegalArgumentException ex){
+                    System.out.println("\n" + ex.getMessage()+ "\n");
+                }
+            }
+        }
+    }
+
+    private void displayList(List<Book> books){
+        if (books.isEmpty()) {
             System.out.println("List is empty");
         } else {
-            records.forEach(System.out::println);
+            System.out.println();
+            DateTimeFormatter format =  DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            books.forEach(book -> System.out.println(book.getName() + " " + book.getAddedAt().format(format)));
         }
     }
 }
--- src/main/java/com/repository/BookRepo.java
+++ src/main/java/com/repository/BookRepo.java
@@ -1,7 +1,35 @@
 package com.repository;
 
+import com.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public class BookRepo {
+
+    private final  JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookRepo(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void createRecord(String name){
+        jdbcTemplate.update("INSERT INTO books (name, added_at) VALUES (?, ?)", name, LocalDateTime.now());
+    }
+
+    public List<Book> getAllRecords(){
+        return jdbcTemplate.query("SELECT * FROM books", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public List<Book> getRecordsByQuery(String query){
+        return jdbcTemplate.query("SELECT * FROM books WHERE name like ?",
+                new BeanPropertyRowMapper<>(Book.class), "%"+query+"%");
+    }
+
 }
