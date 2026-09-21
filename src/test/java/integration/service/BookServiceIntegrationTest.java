7f15430126d8b5a09ddb7f3e3a0d1dc28458adfe
Od1sey <doingmybestsometime@gmail.com>
2026-09-21 03:33:31 +0300

    added shouldOnlyBorrowOneLastBookCopy test

--- src/test/java/integration/service/BookServiceIntegrationTest.java
+++ src/test/java/integration/service/BookServiceIntegrationTest.java
@@ -2,16 +2,18 @@ package integration.service;
 
 import com.entity.Author;
 import com.entity.Book;
+import com.exception.NoAvailableCopiesException;
 import com.repository.AuthorRepo;
 import com.repository.BookRepo;
 import com.service.BookService;
 import integration.IntegrationTestBase;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.concurrent.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -57,4 +59,47 @@ public class BookServiceIntegrationTest extends IntegrationTestBase {
         );
     }
 
+    @Test
+    void shouldOnlyBorrowOneLastBookCopy() throws Exception {
+        Author author = new Author("Initial", "Author");
+        author = authorRepo.save(author);
+        Book book = new Book("Test book", 2025);
+        book.setAuthor(author);
+        book.setCopiesAmount(1);
+
+        Book savedBook = bookService.createRecord(book);
+        Integer bookId = savedBook.getId();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Callable<Boolean> borrowTask = () -> {
+            latch.await();
+            try {
+                bookService.borrow(bookId);
+                return true;
+            } catch (NoAvailableCopiesException e) {
+                return false;
+            }
+        };
+
+        Future<Boolean> firstBorrow = executor.submit(borrowTask);
+        Future<Boolean> secondBorrow = executor.submit(borrowTask);
+        latch.countDown();
+        boolean firstResult = firstBorrow.get();
+        boolean secondResult = secondBorrow.get();
+        executor.shutdown();
+
+        int successfulBorrows = 0;
+        if (firstResult) {
+            successfulBorrows++;
+        }
+        if (secondResult) {
+            successfulBorrows++;
+        }
+
+        assertEquals(1, successfulBorrows);
+        Book bookFromDb = bookRepo.findById(bookId)
+                .orElseThrow();
+        assertEquals(0, bookFromDb.getCopiesAmount());
+    }
 }
